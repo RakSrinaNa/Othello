4b32fa57d4a115ad769d82c577dca5e5a9238c39
MrCraftCod <zerderr@gmail.com>
2014-01-11 17:53:09 +0000

    Attention au nom des fonctions importees, placer_pion n'existe pas, c'est place. Faire respirer le code c'est plus agrèable

--- Interface.py
+++ Interface.py
@@ -2,18 +2,21 @@
 @author: Olivier Froger
 '''
 from tkinter import*
-from Game import*
+from Game import init, getColor, place
 
 def initialisation():
     init()
+    refresh()
+
+def refresh():
     for x in range(0, 8):
         for y in range(0, 8):
             placer_pion(getColor(x, y), x, y)
-def pointeur(event):
-    chaine.configure(text = "Clic détecté en X =" + str(event.x) +\
-                            ", Y =" + str(event.y))
+
 def mettre_pion():
-    placer_pion(noir,((event.x)+50)//50,((event.y)+50)//50)
+    placer(noir, ((event.x) + co) // tcase,((event.y) + co) // tcase)
+    refresh()
+    
 def regles():
     fen1 = Tk()
     fen1.title("Regles du jeu")
@@ -35,14 +38,7 @@ def a_propos():
     fen3.resizable(0, 0)
     fen3.mainloop()
 
-def placer_pion(color, x, y):
-    tc, dec, dec2, col, bl = 50, 25, 5, vert, "black"
-    if color == blanc: col = "white"
-    elif color == noir: col = "black"
-    else: bl = vert
-    Can.create_oval(dec + x * tc + dec2, dec + y * tc + dec2, dec + tc * (x + 1) - dec2, dec + tc * (y + 1) - dec2, fill = col, outline = bl)
-
-vert, blanc, noir, yo, xo ,Comic = "#086126", 1, 2, 2, 8, ("Comic sans MS","9")
+vert, blanc, noir, yo, xo, co, tcase ,Comic = "#086126", 1, 2, 2, 8, 25, 50, ("Comic sans MS","9")
 
 fen = Tk()
 fen.title("Othello")
@@ -67,7 +63,7 @@ fen.config(menu = menubar)
 
 Can = Canvas(fen, bg = vert, height = 435, width = 435)
 Can.place(x = 10, y = 10)
-Can.bind("<Button-1>", pointeur, mettre_pion)
+Can.bind("<Button-1>", mettre_pion)
 Label(Can, text = "A", font = Comic, bg = vert).place(x = 45, y = yo)
 Label(Can, text = "B", font = Comic, bg = vert).place(x = 95, y = yo)
 Label(Can, text = "C", font = Comic, bg = vert).place(x = 145, y = yo)
@@ -85,16 +81,16 @@ Label(Can, text = "6", font = Comic, bg = vert).place(x = xo, y = 290)
 Label(Can, text = "7", font = Comic, bg = vert).place(x = xo, y = 340)
 Label(Can, text = "8", font = Comic, bg = vert).place(x = xo, y = 390)
 Label(fen, text = "Othello", font=  Comic).place(x = 610, y = 5)
-x1, x2, y1, y2 = 25, 25, 25, 25
+x1, x2, y1, y2 = co, co, co, co
 
 for i in range(0, 9):
     Can.create_line(x1, y1, x1 + 400, y1)
     Can.create_line(x2, y2, x2, y2 + 400)
-    y1 += 50
-    x2 += 50
-
-Button(fen,text="Nouvelle partie",command=initialisation).place(x=710,y=1)
+    y1 += tcase
+    x2 += tcase
+    
+Button(fen, text = "Nouvelle partie", command = initialisation).place(x = 710, y = 1)
 chaine = Label(fen)
-chaine.place(x=0,y=450)
+chaine.place(x = 0, y = 450)
 
 fen.mainloop()
