ed025a1468fe6962ae79963f7c22380639781ee5
loky dogma <lokydogma@gmail.com>
2014-01-12 11:07:42 +0000

    Client

--- Reseau.py
+++ Reseau.py
@@ -55,3 +55,31 @@ for client in clients_connectes:
     client.close()
  
 connexion_principale.close()
+
+
+
+
+//////////////// partie client qui teorie pas comme ca mais faut que j arrange ca///////////////
+import socket
+ 
+hote = "localhost"
+port = 12800
+ 
+connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_avec_serveur.connect((hote, port))
+ 
+adv_test="Connexion etablie avec le serveur sur le port {}".format(port)
+ 
+print("\n")
+print(adv_test.upper().center(85))
+msg_a_envoyer = b""
+while msg_a_envoyer != b"fin":
+    msg_a_envoyer = input("> ")
+    msg_a_envoyer = msg_a_envoyer.encode()
+    connexion_avec_serveur.send(msg_a_envoyer)
+    msg_recu = connexion_avec_serveur.recv(1024)
+    msg_recu=msg_recu.decode()
+    print("Notre serveur à bien reçu {}".format(msg_recu)) 
+                 
+print("Fermeture de la connexion")
+connexion_avec_serveur.close()
