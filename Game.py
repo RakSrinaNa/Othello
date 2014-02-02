6f8d1bfb3c75e1f2135bb2d3777d64c7b337d703
MrCraftCod <zerderr@gmail.com>
2014-02-02 16:46:33 +0100

    Details

--- Game.py
+++ Game.py
@@ -4,17 +4,22 @@
 
 from math import copysign
 
+colorOne, colorTwo = 1, 2
+
 def init():
     """
     Permet d'initialiser la grille du jeu
     """
+    #Genere une grille vide
     for y in range(8):
         for x in range(8):
             grille[y][x] = 0
-    grille[3][3] = 1
-    grille[4][3] = 2
-    grille[4][4] = 1
-    grille[3][4] = 2
+            
+    #Initialisation de la grille de depart
+    grille[3][3] = colorOne
+    grille[4][3] = colorTwo
+    grille[4][4] = colorOne
+    grille[3][4] = colorTwo
 
 def getNumberColor(color):
     """
@@ -45,7 +50,7 @@ def getColor(x , y):
     if(x < 0 or x > 7 or y < 0 or y > 7): return 3 #Hors de la grille
     return grille[y][x]
 
-def detectPawn(color, x, y):
+def detectPawn(color, x, y): #TODO: With functions
     """
     Permet de connaitre les pions de meme couleur qui sont sur les memes lignes
         Arguments:
@@ -109,15 +114,20 @@ def getNextPawnDiagonal(color, directionX, directionY, x, y):
     Return:
         Un tableau bi-dimensionnel contenant la position du pion trouve
     """
-    if(abs(directionX) != 1 or abs(directionY) != 1): return
+    if(abs(directionX) != 1 or abs(directionY) != 1): return #Return si une des deux directions differente de 1 ou -1
+    
+    #On se decale d'un dans la diagonale voulue
     x += directionX
     y += directionY
+    
     isEating = False
-    while(getColor(x, y) != 3):
-        if(getColor(x, y) == 0): return
-        if(getColor(x, y) != color): isEating = True
-        if(getColor(x, y) == color and isEating): return (x, y)
-        elif(getColor(x, y) == color and not isEating): return
+    while(getColor(x, y) != 3): #Tant qu'on est pas hors de la grille
+        if(getColor(x, y) == 0): return #Si c'est de l'air on s'arrete, la ligne n'est pas faisable
+        if(getColor(x, y) != color): isEating = True #C'est un pion de l'adversaire, on peut le retourner
+        if(getColor(x, y) == color and isEating): return (x, y) #C'est un de nos pions et un pion adversaire a ete retourne, on le valide
+        elif(getColor(x, y) == color and not isEating): return #C'est un de nos pions mais aucun pion adversaire n'a ete retourne, on arrete
+        
+        #On se decale d'un dans la diagonale voulue
         x += directionX
         y += directionY
     return
@@ -130,15 +140,15 @@ def reverse(color, coordinateBase, coordinateTo):
         coordinateBase -> Les coordonnes du pion origine
         coordinateTo -> Les coordonnes du point d'arrive
     """
-    deltaX = coordinateTo[0] - coordinateBase[0]
-    deltaY = coordinateTo[1] - coordinateBase[1]
-    temporaryX = coordinateBase[0]
-    temporaryY = coordinateBase[1]
-    while(temporaryX != coordinateTo[0] or temporaryY != coordinateTo[1]):
-        grille[temporaryY][temporaryX] = color
-        if(deltaX != 0): temporaryX = int(temporaryX + copysign(1, deltaX))
-        if(deltaY != 0): temporaryY = int(temporaryY + copysign(1, deltaY))
-    grille[coordinateTo[1]][coordinateTo[0]] = color
+    deltaX = coordinateTo[0] - coordinateBase[0] #Decalage en X
+    deltaY = coordinateTo[1] - coordinateBase[1] #Decalage en Y
+    temporaryX = coordinateBase[0] #X de depart
+    temporaryY = coordinateBase[1] #Y de depart
+    while(temporaryX != coordinateTo[0] or temporaryY != coordinateTo[1]): #Tant qu'on est pas arrive a destination
+        grille[temporaryY][temporaryX] = color #On retourne le pion
+        if(deltaX != 0): temporaryX = int(temporaryX + copysign(1, deltaX)) #On se decale d'un en X
+        if(deltaY != 0): temporaryY = int(temporaryY + copysign(1, deltaY)) #On se decale d'un en Y
+    grille[coordinateTo[1]][coordinateTo[0]] = color #On retourne le pion
     
 def hasPawnNext(color, x, y):
     """
@@ -150,17 +160,19 @@ def hasPawnNext(color, x, y):
         True -> Le pion est entoure d'autres pions
         False -> Le pion n'est pas entoure d'autres pions
     """
-    xMin = x - 1
-    xMax = x + 1
-    if(xMin < 0): xMin = 0
-    if(xMax > 7): xMax = 7
-    yMin = y - 1
-    yMax = y + 1
-    if(yMin < 0): yMin = 0
-    if(yMax > 7): yMax = 7
+    xMin = x - 1 #XMin du carre de 3x3 entourrant le pion
+    xMax = x + 1 #XMax du carre de 3x3 entourrant le pion
+    if(xMin < 0): xMin = 0 #Si le XMin sort de la grille, on le remet dedans
+    if(xMax > 7): xMax = 7 #Si le XMax sort de la grille, on le remet dedans
+    yMin = y - 1 #YMin du carre de 3x3 entourrant le pion
+    yMax = y + 1 #YMax du carre de 3x3 entourrant le pion
+    if(yMin < 0): yMin = 0 #Si le YMin sort de la grille, on le remet dedans
+    if(yMax > 7): yMax = 7 #Si le YMax sort de la grille, on le remet dedans
+    
+    #Verification pour chaque case de ce carre de 3x3 entourrant le pion
     for temporaryY in range(yMin, yMax + 1):
         for temporaryX in range(xMin, xMax + 1):
-            if(getColor(temporaryX, temporaryY) != 0 and getColor(temporaryX, temporaryY) != color and (temporaryX != x or temporaryY != y)): return True
+            if(getColor(temporaryX, temporaryY) != 0 and getColor(temporaryX, temporaryY) != color and (temporaryX != x or temporaryY != y)): return True #Si la case n'est pas vide & Si la case n'est pas notre couleur & Si ce n'est pas la ou on joue le pion, c'est bon, il y a pion adverse autour
     return False
 
 def place(color, x , y):
@@ -175,21 +187,25 @@ def place(color, x , y):
         1 -> Case deja occupee
         2 -> Impossible de placer le pion
     """
-    if(getColor(x, y) == 3 or not hasPawnNext(color, x, y)): return 2
-    elif(getColor(x, y) != 0): return 1
+    if(getColor(x, y) == 3 or not hasPawnNext(color, x, y)): return 2 #Si en dehors de la grille ou n'a pas de pion adverse a cote, on ne peut pas jouer
+    elif(getColor(x, y) != 0): return 1 #Si la case n'est aps vide, on ne peut pas jouer
     try:
-        pawnList = detectPawn(color, x, y)
-        if(len(pawnList) < 1): return 2
-        for l in pawnList:
-            reverse(color, (x, y), l)
-    except IndexError: return 2
-    grille[y][x] = color
-    return 0
+        pawnList = detectPawn(color, x, y) #On recupere la liste des lignes a retourner
+        if(len(pawnList) < 1): return 2 #Si la liste est vide, aucun retournement n'est possible, on ne joue pas
+        for l in pawnList: #Pour chque position de fin de ligne
+            reverse(color, (x, y), l) #On retourne cette ligne avec la couleur du joueur
+    except IndexError: return 2 #Si la liste est vide, aucun retournement n'est possible, on ne joue pas
+    grille[y][x] = color #On place le pion joue
+    return 0 #On notifie que le jeu est bon
 
 def showGrid():
+    """
+    Permet d'afficher la grille dans la console
+    """
     for l in grille:
         print(l)
     print()
     
-grille = [[0 for x in range (8)] for x in range (8)]
-init()
+#Lance a l'import des fonctions
+grille = [[0 for x in range (8)] for x in range (8)] #Creation de l'objet grille
+init() #On initialise la grille
